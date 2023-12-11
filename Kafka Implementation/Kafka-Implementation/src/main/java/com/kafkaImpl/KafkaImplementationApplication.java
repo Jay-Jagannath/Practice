c4f6ea6d6ba0433e5ee022252da8fc2f7509e73d
Lalit Behera <140290453+Jay-Jagannath@users.noreply.github.com>
2023-12-11 14:12:17 +0530

    Add files via upload

---
+++ Kafka Implementation/Kafka-Implementation/src/main/java/com/kafkaImpl/KafkaImplementationApplication.java
@@ -0,0 +1,13 @@
+package com.kafkaImpl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaImplementationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaImplementationApplication.class, args);
+	}
+
+}
